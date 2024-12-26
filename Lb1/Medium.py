ce09a3bcc928602444582a9b53a120cba884b807
Ivanov Oleksii <oleksii.ivanov04@gmail.com>
2024-12-26 19:16:42 +0200

    add Medium.py

---
+++ Lb1/Medium.py
@@ -0,0 +1,13 @@
+from telethon.sync import TelegramClient
+
+
+api_id = "1293277"
+api_hash = "13c33b0243b9c6f21e5b435763738924"
+
+with TelegramClient("session_name", api_id, api_hash) as client:
+    participants = client.get_participants("chat_or_channel_name")
+    for participant in participants:
+        print(participant.username)
+
+with TelegramClient("session_name", api_id, api_hash) as client:
+    client.send_message("username_or_chat", "Hello! This is a test message.")
